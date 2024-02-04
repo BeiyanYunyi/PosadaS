6efd96287a064f342a248b8afa3a9a442f0750ec
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2024-02-04 19:53:01 +0800

    ✨ Add webmanifest

---
+++ src/app/manifest.ts
@@ -0,0 +1,15 @@
+import { MetadataRoute } from 'next';
+import { metadata } from './layout';
+
+const manifest = (): MetadataRoute.Manifest => ({
+  name: metadata.title,
+  short_name: metadata.title,
+  description: metadata.description,
+  start_url: '/',
+  display: 'standalone',
+  background_color: '#fff',
+  theme_color: '#000',
+  icons: [{ src: '/icon.svg', sizes: 'any', type: 'image/svg+xml', purpose: 'any' }],
+});
+
+export default manifest;
