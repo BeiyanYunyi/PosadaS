ceb6057c4e85a226b14bd71086b4a3171fff4e92
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2026-09-05 14:26:43 +0800

    chore(utils): 🎨 use pool instead of client

--- src/app/utils/database.ts
+++ src/app/utils/database.ts
@@ -2,18 +2,16 @@ import { cache } from 'react';
 import { relations } from '../../../drizzle/schema';
 
 const getDb = cache(async () => {
-  if (process.env.DATABASE_URL) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL is not defined');
+  }
+
     const { drizzle } = await import('drizzle-orm/node-postgres');
-    const { Client } = await import('pg');
-    const client = new Client({ connectionString: process.env.DATABASE_URL });
+    const { Pool } = await import('pg');
+    const client = new Pool({ connectionString: process.env.DATABASE_URL });
     await client.connect();
     const db = drizzle({ client, relations });
     return db;
-  }
-  const { drizzle } = await import('drizzle-orm/vercel-postgres');
-  const { sql } = await import('@vercel/postgres');
-  const db = drizzle(sql, { relations });
-  return db;
 });
 
 const db = await getDb();
